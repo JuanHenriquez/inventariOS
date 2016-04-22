650fb999ae7e4fcc623016102e36f8888e3698bf
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-04-22 23:09:01 +0000

    Create a gulpfile to run tasks

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+'use strict';
+
+var gulp      = require("gulp"),
+    rename    = require("gulp-rename"),
+    minifyCSS = require("gulp-clean-css"),
+    minifyJS  = require("gulp-uglify");
+    
+gulp.task('update', function(){
+    
+    gulp.src('./bower_components/Materialize/sass/**/', {base: './bower_components/Materialize/sass/'})
+        .pipe(gulp.dest('./dist/css/materialize/'));
+        
+    gulp.src('./bower_components/Materialize/dist/js/materialize.min.js')
+        .pipe(gulp.dest('./dist/js/'));
+        
+    gulp.src('./bower_components/jquery/dist/jquery.min.js')
+        .pipe(gulp.dest('./dist/js/'));
+    
+});
+
+gulp.task('minCss', function() {
+    
+    console.log('Se ha minificado style.css');
+    gulp.src('./dist/css/style.css')
+        .pipe(minifyCSS({debug: true}, function(details){
+            console.log("The file " + details.name + "(" +  details.stats.originalSize + "kb)" + " has been compresed to " + details.stats.minifiedSize + "kb");
+        }))
+        .pipe(rename('style.min.css'))
+        .pipe(gulp.dest('./dist/css/'));
+})
+
+gulp.task('minJs', function() {
+    
+    console.log('Se ha minificado app.js');
+    gulp.src('./dist/js/app.js')
+        .pipe(minifyJS())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest('./dist/js/'))
+    
+});
+
+gulp.task('watch', function() {
+    
+    gulp.watch('./dist/js/app.js', ['minJs']);
+    gulp.watch('./dist/css/style.scss', ['minCss']);
+    
+})
