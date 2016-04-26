df80baf3f5aaa4f32491dcc0cc672b8e762d7124
JuanHenriquez <juanhenriquezzavarse18@gmail.com>
2016-04-26 17:31:51 -0430

    Update admin.php links to the nav options

--- dist/admin.php
+++ dist/admin.php
@@ -1,9 +1,42 @@
+<?php 
+
+    $pageTitle = "admin";
+
+?>
+
 <?php include('inc/header.php') ?>
 
 <main>
-	<div class="row">
-		.col.s
-	</div>
+    <div id="control-panel" class="hide-on-med-and-down">
+        <ul id="menu-items">
+            <li><a href="admin.php?panel=stock">Stock</a></li>
+            <li><a href="admin.php?panel=add">Add</a></li>
+            <li><a href="admin.php?panel=sales">Ventas</a></li>
+        </ul>
+    </div>
+    <div id="control-view">
+        <div class="row">
+            <div class="col s12">
+                <?php 
+                    if($_GET['panel'] == "add"){
+                        include('views/agregar/index.php');
+                    }else if($_GET['panel'] == "stock") {
+                        include('views/inventario/index.php');
+                    }else if($_GET['panel'] == "sales"){
+                        include('views/ventas/index.php');
+                    }else {
+                        
+                    
+                ?>
+                
+                <h1>Welcome to the administration panel</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, repudiandae.</p>
+                
+                <?php } ?>
+            </div>
+        </div>
+
+        <?php include('inc/footer.php') ?>
+    </div>
 </main>
 
-<?php include('inc/footer.php') ?>
