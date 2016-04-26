c2217ffefd49b64c5fd701d208f1509c40b7e19a
JuanHenriquez <juanhenriquezzavarse18@gmail.com>
2016-04-26 17:34:58 -0430

    Include a admin options links in the mobile nav and add new page styles for every page in the app

--- dist/inc/header.php
+++ dist/inc/header.php
@@ -6,6 +6,14 @@
     <title>InventariOS</title>
     <link rel="stylesheet" href="css/owl.carousel.css"/>
     <link rel="stylesheet" href="css/style.min.css"/>
+    
+    <?php if($pageTitle == "admin"): ?>
+    <link rel="stylesheet" href="css/style-admin.min.css"/>
+    <?php elseif($pageTitle == "products"): ?>
+    <link rel="stylesheet" href="css/style-products.min.css"/>
+    <?php endif; ?>
+    
+    
 </head>
 <body>
     
@@ -19,11 +27,19 @@
                         <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
                         <ul class="right hide-on-med-and-down">
                             <li><a href="#">Productos</a></li>
-                            <li><a href="#">Panel de Administración</a></li>
+                            <li><a href="admin.php">Panel de Administración</a></li>
                         </ul>
                         <ul class="side-nav" id="mobile-demo">
                             <li><a href="#">Productos</a></li>
-                            <li><a href="#">Panel de Administración</a></li>
+                            <li><a href="admin.php" data-activates="dropdown">Panel de Administración</a></li>
+                            <?php if($pageTitle == "admin"): ?>
+                                <li class="divider"></li>
+                                <ul class='hide-on-large-only'>
+                                    <li><a href="admin.php?panel=stock">Stock</a></li>
+                                    <li><a href="admin.php?panel=add">Add</a></li>
+                                    <li><a href="admin.php?panel=sales">Sales</a></li>
+                                </ul>
+                            <? endif; ?>
                         </ul>
                     </div>
                 </nav>
