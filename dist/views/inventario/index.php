6eaef2212354a1eecc3ff0b402b8c4e1272a5383
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-04-28 16:37:13 -0430

    Refactor

--- dist/views/inventario/index.php
+++ dist/views/inventario/index.php
@@ -1,3 +1,8 @@
+<?php
+    require('models/producto.php');
+    require('models/almacen.php');
+    require('models/inventario.php');
+?>
 <div class="container">
     <div class="row">
         <div class="col s12">
@@ -5,15 +10,8 @@
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, ipsa!</p>
         </div>
         <div class="col s12">
-            <?php
 
-                if( isset($_GET['action']) && $_GET['action'] == "update"){
-                    include('form_update.php');
-                }else {
-                    include('form_create.php');
-                }
-
-            ?>
+            <?php include('form.php'); ?>
 
         </div> <!-- /.col -->
         <div class="col s12">
