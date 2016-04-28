eedfb3539ecf8b34683bc17058c68baf648acef4
Juan Henriquez <juanhenriquezzavarse18@gmail.com>
2016-04-28 16:35:53 -0430

    Refactor and add new functionality

--- dist/views/agregar/eliminar.js
+++ dist/views/agregar/eliminar.js
@@ -1,19 +1,55 @@
 $(document).ready(function(){
 
-     // Boton tabla
+     // Boton tabla productos
     $('.btn-delete-product').click(function( e ){
         e.preventDefault();
 
         var btn = $(this),
             rowId = btn.parent().siblings('.id-table-product').text();
 
-        var form = $('#form_product'),
-            btn_guardar_cambios = $('#btn_delete_product');
+        var confirmar = confirm("¿Seguro desea elminar el producto?");
+        console.log(rowId);
 
-        btn_guardar_cambios.data("id", rowId);
+        if( confirmar ){
+
+            $.ajax({
+                'type': 'POST',
+                'url': './controllers/productsDeleteController.php',
+                'data': { id: rowId },
+                'success': function(){
+                    console.log("Pa fuera");
+                    var row = btn.parents('.item-row');
+                    row.remove();
+                }
+            });
+
+        }
+    });
+
+     // Boton tabla almacen
+    $('.btn-delete-warehouse').click(function( e ){
+        e.preventDefault();
+
+        var btn = $(this),
+            rowId = btn.parent().siblings('.id-table-warehouse').text();
 
         var confirmar = confirm("¿Seguro desea elminar el producto?");
+        console.log(rowId);
 
+        if( confirmar ){
+
+            $.ajax({
+                'type': 'POST',
+                'url': './controllers/almacenDeleteController.php',
+                'data': { id: rowId },
+                'success': function(){
+                    var row = btn.parents('.item-row');
+                    row.remove();
+                    console.log("Pa fuera");
+                }
+            });
+
+        }
     });
 
 });
